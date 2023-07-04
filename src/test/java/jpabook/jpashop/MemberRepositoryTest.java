6d54906fd601b79995b44f5bba93cc02fa9fc93a
leerobing <105533691+leerobing@users.noreply.github.com>
2023-07-04 15:27:49 +0900

    feat: 프로젝트 생성, 디비 연결 및 테스트

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUserName("MemberA");
+        Long id = memberRepository.save(member);
+
+        //when
+        Member memberA = memberRepository.find(id);
+
+        //then
+        Assertions.assertThat(memberA).isSameAs(member);
+    }
+}
